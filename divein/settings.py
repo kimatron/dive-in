4e93fef3579f090b2ae34931ec16a73aee911f07
kimatron <kimmykim@gmail.com>
2024-02-28 17:52:01 +0000

    Add post model, fix error with allowed hosts

--- divein/settings.py
+++ divein/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -93,9 +93,6 @@ DATABASES = {
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeanyapp.com",
     "https://*.herokuapp.com",
-    "https://localhost:8000",
-    "https://127.0.0.1:8000",
-    "https://<your-local-development-domain>"
 ]
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
