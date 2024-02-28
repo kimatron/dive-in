6bf3b49202b3962641092926cccea1e6d62c2aa0
kimatron <kimmykim@gmail.com>
2024-02-28 17:00:51 +0000

    Add trusted origins and post models

--- divein/settings.py
+++ divein/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
@@ -90,6 +90,13 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.codeanyapp.com",
+    "https://*.herokuapp.com",
+    "https://localhost:8000",
+    "https://127.0.0.1:8000",
+    "https://<your-local-development-domain>"
+]
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
