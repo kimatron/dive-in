fa62e6bf4db89f6499ba4dc3a930e9b67338c73c
kimatron <kimmykim@gmail.com>
2024-02-28 08:37:57 +0000

    Add secret key and databases

--- divein/settings.py
+++ divein/settings.py
@@ -24,8 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2f3x*$#+!jylc_+e)u3$vi4vlki&%fezsm83b(m0vm17s(1&n3'
+# SECRET_KEY = 'django-insecure-2f3x*$#+!jylc_+e)u3$vi4vlki&%fezsm83b(m0vm17s(1&n3'
 
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
