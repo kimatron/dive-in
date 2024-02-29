135df4335979e8fb1b422ba80b557d1cb0619ad6
kimatron <kimmykim@gmail.com>
2024-02-29 22:29:37 +0000

    Add comment model

--- blog/models.py
+++ blog/models.py
@@ -16,3 +16,14 @@ class Post(models.Model):
   status = models.IntegerField(choices=STATUS, default=0)
   excerpt = models.TextField(blank=True)
   updated_on = models.DateTimeField(auto_now=True)
+  
+  
+# Define Comment as a separate class
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
