4362c6a0ccf26d25b21cecece7699ca48170c77c
kimatron <kimmykim@gmail.com>
2024-02-29 21:36:54 +0000

    Comment out comment model to test deploy

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from .models import Post, Comment
-
+from .models import Post #, Comment
 # Register your models here.
 
 admin.site.register(Post)
-admin.site.register(Comment)
+# admin.site.register(Comment)
