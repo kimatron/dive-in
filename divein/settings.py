06fb23215e186d81fedaae61c233dfa63c691d90
kimatron <kimmykim@gmail.com>
2024-03-01 12:49:07 +0000

    Install summernote and update files

--- divein/settings.py
+++ divein/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'django_summernote'
 ]
 
 MIDDLEWARE = [
--- divein/urls.py
+++ divein/urls.py
@@ -21,4 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("", include("blog.urls"), name="blog-urls"),
 ]
