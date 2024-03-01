98b4062a5bcb74c8ef9c9d59ddcad36cb1e63fca
kimatron <kimmykim@gmail.com>
2024-03-01 11:51:44 +0000

    Change to generic post view for deployment

--- blog/views.py
+++ blog/views.py
@@ -3,4 +3,5 @@ from django.views import generic
 from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
-   model = Post
+    queryset = Post.objects.all()
+    template_name = "post_list.html"
