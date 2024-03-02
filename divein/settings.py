b7cda036858b8d1db46b00b739f0610190928b28
kimatron <kimmykim@gmail.com>
2024-03-02 00:47:24 +0000

    Add base html code to build from

--- divein/settings.py
+++ divein/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'divein.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
