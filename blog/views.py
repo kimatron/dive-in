44efdd09f495d48d636b37ebda45046036ab424e
kimatron <kimmykim@gmail.com>
2024-03-02 01:09:51 +0000

    Add initial style sheet for static use

--- blog/views.py
+++ blog/views.py
@@ -4,4 +4,5 @@ from .models import Post
 # Create your views here.
 class PostList(generic.ListView):
     queryset = queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "post_list.html"
+    template_name = "blog/index.html"
+paginate_by = 5
