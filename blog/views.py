8cf33ddc369925ae7c58d5f12cadacef71722cdf
kimatron <kimmykim@gmail.com>
2024-03-13 05:10:40 +0000

    Refactored post_detail view in blog app to use method chaining for queryset operations

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 from django.views import generic
 from .models import Post
+from django.shortcuts import get_object_or_404
 # Create your views here.
 class PostList(generic.ListView):
     queryset = queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "blog/index.html"
     paginate_by = 5
+
+
+
+def post_detail(request, slug):
+    """
+    Display an individual :model:`blog.Post`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`blog.Post`.
+
+    **Template:**
+
+    :template:`blog/post_detail.html`
+    """
+
+    post = Post.objects.filter(status=1).get(slug=slug)
+    
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post},
+    )
