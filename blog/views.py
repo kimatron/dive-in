c75b3d0e5b60cdb6fdaa27469cbc77df4dddf825
kimatron <kimmykim@gmail.com>
2024-04-25 14:42:10 +0100

    Add confirmation message when user posts comment

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib import messages
 from .models import Post
 from django.shortcuts import get_object_or_404
 from .models import Post
@@ -30,7 +31,19 @@ def post_detail(request, slug):
     post = get_object_or_404(Post, status=1, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
-    comment_form = CommentForm()
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.author = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and will be approved shortly, as long as you"re not a rudey'
+            )
+    else:
+        comment_form = CommentForm()
 
     return render(
         request,
