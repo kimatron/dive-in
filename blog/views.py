6184d01a7b49d62187f4b1de4ea6e668acd2070b
kimatron <kimmykim@gmail.com>
2024-04-25 12:47:00 +0100

    Add comment views to blog posts

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,12 @@ from django.views import generic
 from .models import Post
 from django.shortcuts import get_object_or_404
 # Create your views here.
+
+
 class PostList(generic.ListView):
     queryset = queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "blog/index.html"
     paginate_by = 5
-
 
 
 def post_detail(request, slug):
@@ -24,10 +25,16 @@ def post_detail(request, slug):
     :template:`blog/post_detail.html`
     """
 
-    post = Post.objects.filter(status=1).get(slug=slug)
-    
+    post = get_object_or_404(Post, status=1, slug=slug)
+    comments = post.comments.all().order_by("-created_on")
+    comment_count = post.comments.filter(approved=True).count()
+
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post},
+        {
+            "post": post,
+            "comments": comments,
+            "comment_count": comment_count,
+        },
     )
